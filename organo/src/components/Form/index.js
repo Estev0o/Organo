959e2ba82548881ac7c244f9ae7ea11918f5737e
Vinicius Estevo <viniestevo33@gmail.com>
2024-06-18 14:05:40 -0300

    use useState of react

--- organo/src/components/Form/index.js
+++ organo/src/components/Form/index.js
@@ -2,8 +2,10 @@ import List from "../DropDownList";
 import TextForm from "../textForm";
 import Button from "../button";
 import "./formulario.css";
+import { useState } from "react";
 
-const Formulario = () => {
+
+const Formulario = (props) => {
 
     const teams = [
         'Programação',
@@ -17,16 +19,52 @@ const Formulario = () => {
 
     const save = (event) => {
         event.preventDefault()
+        props.aoColaboradorCadastrado({
+            nome: nome,
+            cargo: cargo,
+            imagem: imagem,
+            time: time
+        })
     }
+
+    const [nome, setNome] = useState('')
+    const [cargo, setCargo] = useState('')
+    const [imagem, setImagem] = useState('')
+    const [time, setTime] = useState('')
 
     return (
         <section className="form">
             <form onSubmit={save}>
                 <h2>Preencha os dados para criar os cards do colaborador</h2>
-                <TextForm obrigatorio={true} label="Nome" placeholder="Digite seu nome" />
-                <TextForm obrigatorio={true} label="Cargo" placeholder="Digite seu cargo" />
-                <TextForm label="Imagem" placeholder="Digite o endereço da imagem" />
-                <List obrigatorio={true} label="Time" itens={teams}/>
+                <TextForm 
+                    obrigatorio={true} 
+                    label="Nome" 
+                    placeholder="Digite seu nome" 
+                    valueR={nome}
+                    OnAlter={valueR => setNome(valueR)}
+                />
+
+                <TextForm 
+                    obrigatorio={true} 
+                    label="Cargo" 
+                    placeholder="Digite seu cargo"
+                    valueR={cargo}
+                    OnAlter={valueR => setCargo(valueR)}
+                />
+                
+                <TextForm 
+                    label="Imagem" 
+                    placeholder="Digite o endereço da imagem"
+                    valueR={imagem}
+                    OnAlter={valueR => setImagem(valueR)} 
+                />
+                <List 
+                    obrigatorio={true} 
+                    label="Time" 
+                    itens={teams}
+                    valueR={time}
+                    OnAlter={valueR => setTime(valueR)}
+                />
                 <Button>
                     Criar card
                 </Button>
