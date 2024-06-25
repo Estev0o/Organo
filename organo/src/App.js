9f77b4bbdfcdb44409ebe16bd7cacfc5377cf78b
Vinicius Estevo <viniestevo33@gmail.com>
2024-06-25 20:20:30 -0300

    ident  code, filter colaboradores to respective teams

--- organo/src/App.js
+++ organo/src/App.js
@@ -7,44 +7,44 @@ function App(props) {
 
   const teams = [
     {
-      name:'Programação',
-      primaryColor:'#57c278',
+      name: 'Programação',
+      primaryColor: '#57c278',
       secondaryColor: '#d9f7e9',
     },
     {
-      name:'Front-End',
-      primaryColor:'#82cffa',
+      name: 'Front-End',
+      primaryColor: '#82cffa',
       secondaryColor: '#e8f8ff',
     },
     {
-      name:'Data Sciemce',
-      primaryColor:'#a6d157',
+      name: 'Data Sciemce',
+      primaryColor: '#a6d157',
       secondaryColor: '#f0f8e2',
-    },{
-      name:'Devops',
-      primaryColor:'#e06b69',
+    }, {
+      name: 'Devops',
+      primaryColor: '#e06b69',
       secondaryColor: '#fde7e8',
     },
     {
-      name:'UX e Design',
-      primaryColor:'#db6ebf',
+      name: 'UX e Design',
+      primaryColor: '#db6ebf',
       secondaryColor: '#fae9f5',
     },
     {
-      name:'Mobile',
-      primaryColor:'#ffba05',
+      name: 'Mobile',
+      primaryColor: '#ffba05',
       secondaryColor: '#fff5d9',
     },
     {
-      name:'Inovação e Gestão',
-      primaryColor:'#ff8a29',
+      name: 'Inovação e Gestão',
+      primaryColor: '#ff8a29',
       secondaryColor: '#ffeedf',
     }
   ]
 
   const [colaboradores, setColaboradores] = useState([])
   const aoNovoColaboradorAdicionado = (colaborador) => {
-    console.log(colaborador)
+
     setColaboradores([...colaboradores, colaborador])
   }
 
@@ -52,9 +52,13 @@ function App(props) {
     <div className="App">
       <Banner />
       <Formulario times={teams.map(teams => teams.name)} aoColaboradorCadastrado={colaborador => aoNovoColaboradorAdicionado(colaborador)} />
-      {teams.map(team => <Team key={team.name} name={team.name} 
-      primaryColor={team.primaryColor}
-      secondaryColor={team.secondaryColor}/>)}
+      {teams.map(team => <Team
+        key={team.name} 
+        name={team.name}
+        primaryColor={team.primaryColor}
+        secondaryColor={team.secondaryColor} 
+        colaboradores={colaboradores.filter(colaborador => colaborador.time === team.name)}
+        />)}
     </div>
   );
 }
