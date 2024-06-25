5073dc2555fa149bae95bbde22a7b6ad11786f4d
Vinicius Estevo <viniestevo33@gmail.com>
2024-06-25 16:52:18 -0300

    remove array of teams

---
+++ organo/src/components/Employees/index.js
@@ -0,0 +1,17 @@
+import './employees.css'
+
+const Employees = () => {
+    return (
+        <div>
+            <div>
+                <img src='https://github.com/Estev0o.png' alt="" />
+            </div>
+            <div>
+                <h4>Vinicius</h4>
+                <h5>.NET</h5>
+            </div>
+        </div>
+    )
+}
+
+export default Employees
